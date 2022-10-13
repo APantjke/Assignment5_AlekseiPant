713232387af5c6654159d650ada196b4de799494
Aleksei Pant <apantjke@ut.ee>
2022-10-13 15:54:44 +0300

    Добавьте файлы проекта.

---
+++ Assignment5_AlekseiPant/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Assignment5_AlekseiPant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.enterNumber = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click to generate a random number from 1 to 100";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(318, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.generateButton);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(80, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(254, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Your number was generated successfully.";
+            this.label2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(318, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Check";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.checkButton);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(20, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Try go guess the number";
+            this.label3.Visible = false;
+            // 
+            // enterNumber
+            // 
+            this.enterNumber.Location = new System.Drawing.Point(181, 99);
+            this.enterNumber.Name = "enterNumber";
+            this.enterNumber.Size = new System.Drawing.Size(100, 20);
+            this.enterNumber.TabIndex = 5;
+            this.enterNumber.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(60, 140);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(300, 70);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 241);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.enterNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox enterNumber;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Assignment5_AlekseiPant/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment5_AlekseiPant
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int number = 0;
+        int guesses = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+            label3.Visible = true;
+            enterNumber.Visible = true;
+            textBox2.Visible = true;
+            button2.Visible = true;
+            number = random.Next(1, 100);
+            guesses = 0;
+        }
+
+        private void checkButton(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(enterNumber.Text);
+            guesses += 1;
+            if (i == number)
+            {
+                textBox2.Text = "Congratulations, you guessed the number " + number + "!" + "\r" + "\n" + "You guessed " + guesses + " times";
+                enterNumber.Text = "";
+                guesses = 0;
+            }
+            else if (i < number)
+            {
+                textBox2.Text = "Too low, try again" + "\r" + "\n" + "You guessed " + guesses + " times";
+            }
+            else
+            {
+                textBox2.Text = "Too high, try again" + "\r" + "\n" + "You guessed " + guesses + " times";
+            }
+        }
+    }
+}
